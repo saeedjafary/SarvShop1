6dff1ea6c4ead71e5aa0ccde15017cca1b881a58
nowpardaz1 <saeedjafarymail@gmail.com>
2019-12-17 21:01:56 +0330

    add native base

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import { StyleSheet,View, Text  } from 'react-native'; 
 import { connect } from "react-redux"
+import Server from './Server.js'
 
 class Home extends React.Component {   
   constructor(props){   
     super(props);    
-    
-
-    
-   
+      
+        this.Server = new Server();
+    this.state = {
+      username:'9656',
+      password:'1',
+      result:'ssssssss'
+    }
 
   }  
   componentDidMount() {
- 
+ let that = this;
+   
+    let SCallBack = function(response){
+      //alert(2)
+      that.setState({
+        result:'OK1'  
+      })
+     console.warn(response)   
+    } 
+    let ECallBack = function(error){
+     alert(error)   
+    }  
+        
+   this.Server.send("https://marketapi.sarvapps.ir/MainApi/getuser",{'username':'123','password':'123'},SCallBack,ECallBack) 
   
   }
   
@@ -22,28 +39,21 @@ class Home extends React.Component {
  
   }
  
-  render() {
+  render() { 
 
            
     return (   
                 
     <View>
-    <Text>Home Screan</Text>
+
+      <Text style={{marginTop:100}}>{this.state.result}</Text>
     </View>
            
     );
   }
 }
 
-const styles = StyleSheet.create({
-  
-  label:{
-    fontFamily:'IRANSansMobile',
-    fontSize:12     
 
-      
-  }
-});
 function mapStateToProps(state) {        
   return {
     username : state.username
--- components/Server.js
+++ components/Server.js
@@ -15,21 +15,30 @@ export default class Server extends React.Component {
   send(url,params,SuccessCallBack,ErrorCallBack){
     axios.post(url, params)
     .then(function (response) {
-      SuccessCallBack(response);
+      SuccessCallBack(response); 
     })
     .catch(function (error) {
       ErrorCallBack(error);
     });
+   /* fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json; charset=utf-8',
+        'Content-Type': 'application/json',
+      }
+    }) 
+    .then((response) => response.json())
+    .then((response) => {
+          alert(1)      
+          SuccessCallBack(response.d);        
+      
+    })
+    .catch((error) => {
+      ErrorCallBack(error);
+    });*/
  
  }
 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
